f572ac658479da5ae57bf19dfbc13029e7cf2938
carlosjosetri <00082217@uca.edu.sv>
2020-06-10 14:25:36 -0600

    terminado

---
+++ codigo/cmake-build-debug/CMakeFiles/_TSC_Laboratorio7_NS2SD_Source_Code_master.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_TSC_Laboratorio7_NS2SD_Source_Code_master.dir/main.cpp.obj"
+  "_TSC_Laboratorio7_NS2SD_Source_Code_master.pdb"
+  "_TSC_Laboratorio7_NS2SD_Source_Code_master.exe"
+  "_TSC_Laboratorio7_NS2SD_Source_Code_master.exe.manifest"
+  "lib_TSC_Laboratorio7_NS2SD_Source_Code_master.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_TSC_Laboratorio7_NS2SD_Source_Code_master.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ codigo/cmake-build-debug/CMakeFiles/_TSC_Laboratorio7_NS2SD_Source_Code_master.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for _TSC_Laboratorio7_NS2SD_Source_Code_master.
+# This may be replaced when dependencies are built.
